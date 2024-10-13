54a8f60d6d11e834b45f96153863af312f9ccdb8
Thuch_o_o <91382929+Thadthon08@users.noreply.github.com>
2024-10-13 10:29:14 +0700

    Imp notifly

--- frontend/src/utils/toastUtils.ts
+++ frontend/src/utils/toastUtils.ts
@@ -2,8 +2,8 @@ import { toast } from "react-toastify";
 
 export const showToast = (message: string, type: "success" | "error") => {
   if (type === "success") {
-    toast.success(message, { position: "top-right" });
+    toast.success(message, { position: "top-right", autoClose: 2000 });
   } else if (type === "error") {
-    toast.error(message, { position: "top-right" });
+    toast.error(message, { position: "top-right", autoClose: 2000 });
   }
 };
