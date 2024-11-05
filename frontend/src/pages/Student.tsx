06ca234be34ae2e6e1e701e464b0688bda7ba25f
Thuch_o_o <91382929+Thadthon08@users.noreply.github.com>
2024-11-05 15:43:01 +0700

    add size scan

--- frontend/src/pages/Student.tsx
+++ frontend/src/pages/Student.tsx
@@ -166,7 +166,7 @@ export default function StudentDashboard() {
       html5QrCode.current
         .start(
           { facingMode: "environment" },
-          { fps: 30, qrbox: { width: 250, height: 250 } },
+          { fps: 30, qrbox: { width: 400, height: 400 } },
           handleScan,
           (errorMessage) =>
             console.error("QR Code scanning error:", errorMessage)
